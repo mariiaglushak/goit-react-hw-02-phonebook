e85b87b132adbd0eeb37c233f231d259bdf598c7
mariiaglushak <90639229+mariiaglushak@users.noreply.github.com>
2023-10-14 00:41:47 +0300

    правки

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -33,6 +33,8 @@ class ContactForm extends Component {
           value={name}
           type="text"
           name="name"
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
           required="required"
         ></Input>
         <Input
@@ -40,6 +42,8 @@ class ContactForm extends Component {
           onChange={this.handleFormInput}
           type="tel"
           name="number"
+          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
           required="required"
         ></Input>
         <AddContactBtn text="add contact" />
