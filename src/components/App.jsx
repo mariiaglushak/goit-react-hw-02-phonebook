b1947b6aa31f14768e1c2fb057f1ae8857d30d28
mariiaglushak <90639229+mariiaglushak@users.noreply.github.com>
2023-10-13 23:13:21 +0300

    правка

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -14,6 +14,8 @@ export class App extends Component {
       { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
     ],
     filter: '',
+    name: '',
+    number: '',
   };
 
   handlerFormSubmit = ({ name, number }) => {
